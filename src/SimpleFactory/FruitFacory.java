a2d959f0c9b3efc60ac89b8e8c4ad74653648919
tengyida <15846534771@163.com>
2018-07-11 18:10:12 +0800

    update code

--- src/SimpleFactory/FruitFacory.java
+++ src/SimpleFactory/FruitFacory.java
@@ -1,7 +1,16 @@
 package SimpleFactory;
 
 public class FruitFacory {
-	public static Fruit getFruit(String type) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static Fruit getFruit(String type)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+//		if (type.equalsIgnoreCase("apple")) {
+//			return Apple.class.newInstance();
+//		} else if (type.equalsIgnoreCase("banana")) {
+//			return Banana.class.newInstance();
+//		} else {
+//			System.out.println("输入的类型有误");
+//		}
+
 		Class fruit = Class.forName(type);
 		return (Fruit) fruit.newInstance();
 	}
--- src/SimpleFactory/MainClass.java
+++ src/SimpleFactory/MainClass.java
@@ -1,5 +1,9 @@
 package SimpleFactory;
-
+/**
+ *一.简单工厂模式又叫静态工厂方法模式，通过专门定义的类创建其他类的实例，被创建的实例通常具有相同的父类
+ *二.工厂角色,抽象角色(工厂创建实例的父类)，具体产品角色(工厂创建的实例)
+ *三.
+ */
 public class MainClass {
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		Fruit apple = FruitFacory.getFruit("SimpleFactory.Apple");//包名+类名
